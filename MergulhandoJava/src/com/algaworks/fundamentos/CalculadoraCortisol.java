c7621ddf6bde46757199157321d509563faa756b
IranSS <iranildosousa1234@gmail.com>
2023-08-14 14:21:13 -0300

    MAIN

---
+++ MergulhandoJava/src/com/algaworks/fundamentos/CalculadoraCortisol.java
@@ -0,0 +1,25 @@
+package com.algaworks.fundamentos;
+
+public class CalculadoraCortisol {
+	
+	public static void main(String[] args) {
+		double cortisol = 2;
+		
+//		boolean resultadoNormal = cortisol >= 6.0 && cortisol <= 18.4;
+//		boolean resultadoAnormal = cortisol < 6.0 || cortisol > 18.4;
+		
+//		System.out.println("Cortisol normal: " + resultadoNormal);
+//		System.out.println("Cortisol anormal: " + resultadoAnormal);	
+		
+		if(cortisol >= 6.0 && cortisol <= 18.4) {
+			System.out.println("Cortisol normal");
+		}
+		else if(cortisol > 18.4) {
+			System.out.println("Cortisol alto");
+		}
+		else {
+			System.out.println("Cortisol baixo");
+		}
+		
+	}
+}
